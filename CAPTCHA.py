cadf7ecad44d07c1678b419ac5ca38d65e79ae23
Ankur13997 <54276967+Ankur13997@users.noreply.github.com>
2019-10-24 18:58:15 +0530

    ADDED NEW FEATURES

--- CAPTCHA.py
+++ CAPTCHA.py
@@ -1,5 +1,5 @@
 import random
-c=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
+c='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 l=len(c)
 
 m=random.randrange(l+1)
@@ -13,6 +13,6 @@ y=input("ENTER CAPTCHA: ")
 print("")
 print("")
 if x!=y:
-    print("INVALID CAPTCHA")
+    print("!!! INVALID CAPTCHA !!!")
 else:
-    print("VALID CAPTCHA")
+    print("### VALID CAPTCHA ###")
